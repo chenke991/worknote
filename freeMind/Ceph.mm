cbe6bab6ea998165048207d0268a96dd262fd30d
whoami <chenke@2xi.com>
2017-09-08 11:29:46 +0800

    commit at 2017年 09月 08日 星期五 11:29:44 CST

---
+++ freeMind/Ceph.mm
@@ -0,0 +1,18 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1480212280036" ID="ID_930366294" MODIFIED="1480212428350" STYLE="bubble" TEXT="ceph">
+<node CREATED="1480212288660" ID="ID_506717697" MODIFIED="1480212428350" POSITION="right" STYLE="bubble" TEXT="&#x89d2;&#x8272;">
+<node CREATED="1480212302624" ID="ID_299205990" MODIFIED="1480212428350" STYLE="bubble" TEXT="OSDs">
+<node CREATED="1480212324723" HGAP="23" ID="ID_603919199" MODIFIED="1480212437696" STYLE="bubble" TEXT="&#x6570;&#x91cf;&#xff1a;&#x4e24;&#x4e2a;&#x53ca;&#x4ee5;&#x4e0a;" VSHIFT="-23"/>
+<node CREATED="1480212359790" ID="ID_1436794843" MODIFIED="1480212434026" STYLE="bubble" TEXT="&#x5b58;&#x50a8;&#x6570;&#x636e;&#xff0c;&#x5904;&#x7406;&#x6570;&#x636e;&#x590d;&#x5236;&#xff0c;&#x6062;&#x590d;&#xff0c;&#x56de;&#x586b;&#xff0c;&#x91cd;&#x65b0;&#x8c03;&#x6574;&#xff0c;&#x5e76;&#x901a;&#x8fc7;&#x68c0;&#x67e5;&#x5176;&#x5b83;Ceph OSD&#x5b88;&#x62a4;&#x7a0b;&#x5e8f;&#x4f5c;&#x4e3a;&#x4e00;&#x4e2a;&#x5fc3;&#x8df3; &#x5411;Ceph&#x7684;&#x76d1;&#x89c6;&#x5668;&#x62a5;&#x544a;&#x4e00;&#x4e9b;&#x68c0;&#x6d4b;&#x4fe1;&#x606f;" VSHIFT="25"/>
+</node>
+<node CREATED="1486786654755" ID="ID_1530062468" MODIFIED="1486786660800" TEXT="Monitor"/>
+<node CREATED="1486786661129" HGAP="19" ID="ID_443814522" MODIFIED="1486786691222" TEXT="MDS" VSHIFT="64">
+<node CREATED="1486786665128" ID="ID_1291745252" MODIFIED="1486786684913" TEXT="&#x4f7f;&#x7528;Cephfs&#x65f6;&#x624d;&#x5b89;&#x88c5;"/>
+</node>
+</node>
+<node CREATED="1480212462048" HGAP="-66" ID="ID_1082047900" MODIFIED="1480212500351" POSITION="left" STYLE="bubble" TEXT="&#x9ed8;&#x8ba4;&#x503c;" VSHIFT="-121">
+<node CREATED="1480212474892" ID="ID_247211637" MODIFIED="1480212510143" STYLE="bubble" TEXT="2&#x4e2a;&#x5907;&#x4efd;,&#x53ef;&#x8c03;"/>
+</node>
+</node>
+</map>
---
+++ freeMind/筹码分布.mm
@@ -0,0 +1,48 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1479867697410" ID="ID_1001336363" MODIFIED="1479868787169" STYLE="bubble" TEXT="&#x7b79;&#x7801;&#x5206;&#x5e03;">
+<font NAME="SansSerif" SIZE="16"/>
+<hook NAME="accessories/plugins/HierarchicalIcons.properties"/>
+<node CREATED="1479867723038" HGAP="40" ID="ID_1505450901" MODIFIED="1479868803781" POSITION="right" STYLE="bubble" TEXT="ASR &#x6d6e;&#x7b79;&#x6307;&#x6807;" VSHIFT="27">
+<font ITALIC="true" NAME="SansSerif" SIZE="16"/>
+<node CREATED="1479868073755" HGAP="19" ID="ID_310747939" MODIFIED="1479868829403" STYLE="bubble" TEXT="&#x542b;&#x4e49;&#xff1a;&#x5f53;&#x524d;&#x80a1;&#x4ef7;&#xff08;&#x6536;&#x76d8;&#x4ef7;&#xff09;&#x4e0a;&#x4e0b;7.5%&#x8fd9;&#x4e2a;&#x4ef7;&#x683c;&#x7a7a;&#x95f4;&#xff08;&#x5171;15%&#xff09;&#x6240;&#x542b;&#x7684;&#x7b79;&#x7801;&#x6570;&#x91cf;" VSHIFT="-26">
+<icon BUILTIN="info"/>
+</node>
+<node CREATED="1479868146837" HGAP="38" ID="ID_1509722006" MODIFIED="1479868742875" TEXT="&#x4f5c;&#x7528;&#xff1a;" VSHIFT="-36">
+<font ITALIC="true" NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="info"/>
+<node COLOR="#11822f" CREATED="1479868361479" ID="ID_652180582" MODIFIED="1479868937056" TEXT="&#x53d1;&#x73b0;&#x4e3b;&#x529b;&#x4ee5;&#x53ca;&#x4e3b;&#x529b;&#x62c9;&#x5347;&#x524d;&#x7b79;&#x7801;&#x9501;&#x5b9a;&#x60c5;&#x51b5;"/>
+<node COLOR="#11822f" CREATED="1479868365429" HGAP="23" ID="ID_538075298" MODIFIED="1479868930304" TEXT="&#x6570;&#x503c;&#x8d8a;&#x5927;&#x8d8a;&#x5bc6;&#x96c6;&#xff0c;&#x6570;&#x503c;&#x8d8a;&#x5c0f;&#x8d8a;&#x5206;&#x6563;" VSHIFT="25"/>
+</node>
+<node CREATED="1479868422578" HGAP="23" ID="ID_1504774039" MODIFIED="1479868755134" TEXT="&#x4f7f;&#x7528;&#xff1a;" VSHIFT="-6">
+<icon BUILTIN="hourglass"/>
+<node COLOR="#fb03fd" CREATED="1479868437554" HGAP="18" ID="ID_1497133054" MODIFIED="1479868872398" TEXT="60&#x4ee5;&#x4e0a;&#x5c31;&#x7b97;&#x5bc6;&#x96c6;&#x3002;&#x6570;&#x503c;&#x8d8a;&#x5927;&#x5bc6;&#x96c6;&#x8d8a;&#x597d;&#x3002;" VSHIFT="-9"/>
+<node CREATED="1479868460053" HGAP="21" ID="ID_282666436" MODIFIED="1479868699212" TEXT="&#x901a;&#x8fc7;&#x884c;&#x60c5;&#x6d4f;&#x89c8;&#x6392;&#x5e8f;&#x67e5;&#x627e;" VSHIFT="-3"/>
+<node CREATED="1479868615361" HGAP="28" ID="ID_210226780" MODIFIED="1479868696764" TEXT="&#x901a;&#x8fc7;&#x6307;&#x6807;&#x7a97;&#x6307;&#x6807;&#x76d1;&#x63a7;" VSHIFT="4"/>
+<node CREATED="1479868629008" ID="ID_209453834" MODIFIED="1479868693364" TEXT="&#x7b79;&#x7801;&#x6eda;&#x52a8;&#x62c9;&#x5347;" VSHIFT="8">
+<node CREATED="1479868636600" ID="ID_1585405786" MODIFIED="1479868645134" TEXT="&#x6570;&#x503c;&#x4ece;&#x5927;&#x5230;&#x5c0f;&#x8fc5;&#x901f;&#xff0c;&#x4ece;&#x5c0f;&#x5230;&#x5927;&#x4e5f;&#x8fc5;&#x901f;"/>
+</node>
+<node CREATED="1479868648457" HGAP="21" ID="ID_1044031964" MODIFIED="1479868692084" TEXT="&#x7b79;&#x7801;&#x9501;&#x4ed3;&#x62c9;&#x5347;" VSHIFT="10">
+<node CREATED="1479868663216" ID="ID_1150819579" MODIFIED="1479868675874" TEXT="&#x6570;&#x503c;&#x4ece;&#x5927;&#x5230;&#x5c0f;&#x8fc5;&#x901f;&#xff0c;&#x4ece;&#x5c0f;&#x5230;&#x5927;&#x7f13;&#x6162;"/>
+</node>
+</node>
+<node CREATED="1479868548167" ID="ID_987050181" MODIFIED="1479868761548" TEXT="&#x6ce8;&#x610f;&#xff1a;" VSHIFT="24">
+<icon BUILTIN="clanbomber"/>
+<node COLOR="#fb03fd" CREATED="1479868550409" ID="ID_1798034937" MODIFIED="1479868906400" TEXT="&#x7b79;&#x7801;&#x5bc6;&#x96c6;&#x4e0d;&#x4e00;&#x5b9a;&#x62c9;&#x5347;&#xff0c;&#x4f46;&#x725b;&#x80a1;&#x62c9;&#x5347;&#x524d;&#x7b79;&#x7801;&#x4e00;&#x5b9a;&#x8981;&#x5bc6;&#x96c6;&#x3002;"/>
+</node>
+</node>
+<node CREATED="1479867774167" HGAP="51" ID="ID_1747996228" MODIFIED="1479885330245" POSITION="right" STYLE="bubble" TEXT="SSRP &#x7b79;&#x7801;&#x5cf0;&#x4ef7;&#x4f4d;&#x6307;&#x6807;" VSHIFT="56">
+<node CREATED="1479868998049" HGAP="30" ID="ID_1542124054" MODIFIED="1479869084123" TEXT="&#x542b;&#x4e49;&#xff1a;&#x96c6;&#x4e2d;&#x7b79;&#x7801;&#x5cf0;&#x503c;&#x4ef7;&#x4f4d;&#x6307;&#x6807;" VSHIFT="-5"/>
+<node CREATED="1479869404545" HGAP="29" ID="ID_710042842" MODIFIED="1479869504558" TEXT="&#x6e90;&#x7801;&#x516c;&#x5f0f;&#xff1a;" VSHIFT="2">
+<node CREATED="1479869413259" ID="ID_1196493306" MODIFIED="1479869493382" TEXT="SSRP: DMA(C, V/CAPITAL);#&#x6765;&#x6e90;&#x81ea;&#x7f51;&#x4e0a;"/>
+</node>
+<node CREATED="1479869020633" HGAP="22" ID="ID_210099276" MODIFIED="1479869502206" TEXT="&#x4f5c;&#x7528;&#xff1a;&#x8ffd;&#x8e2a;&#x96c6;&#x4e2d;&#x7b79;&#x7801;&#x5cf0;&#x503c;&#x4ef7;&#x4f4d;" VSHIFT="14">
+<node COLOR="#ec11bc" CREATED="1479869104108" HGAP="22" ID="ID_300247335" MODIFIED="1479869580572" TEXT="&#x8d8a;&#x5927;&#x7b79;&#x7801;&#x5cf0;&#x503c;&#x4ef7;&#x4f4d;&#x8d8a;&#x9ad8;" VSHIFT="-18"/>
+<node COLOR="#ec11bc" CREATED="1479869117082" HGAP="25" ID="ID_1583476634" MODIFIED="1479869582509" TEXT="&#x8d8a;&#x5c0f;&#x7b79;&#x7801;&#x5cf0;&#x503c;&#x4ef7;&#x4f4d;&#x8d8a;&#x4f4e;" VSHIFT="-2"/>
+<node CREATED="1479869138609" HGAP="15" ID="ID_1397443359" MODIFIED="1479869570413" TEXT="&#x7b2c;&#x4e09;&#x4e2a;&#x53c2;&#x6570;&#x662f;&#x7edf;&#x8ba1;&#x7684;&#x4ef7;&#x683c;&#x533a;&#x95f4;&#x53c2;&#x6570;" VSHIFT="19"/>
+</node>
+</node>
+<node CREATED="1479867792153" HGAP="26" ID="ID_1333149710" MODIFIED="1479868133457" POSITION="right" STYLE="bubble" TEXT="CYS &#x5e02;&#x573a;&#x76c8;&#x4e8f;&#x6307;&#x6807;" VSHIFT="67"/>
+<node CREATED="1479867727823" HGAP="4" ID="ID_1549422144" MODIFIED="1479868133457" POSITION="right" STYLE="bubble" TEXT="PAVE &#x5f3a;&#x529b;&#x7b79;&#x7801;&#x5f15;&#x529b;&#x6307;&#x6807;" VSHIFT="86"/>
+</node>
+</map>
