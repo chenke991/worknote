e4aebc50fd491e3f9d7298300d6042b70b95ce85
whoami <chenke@2xi.com>
2017-09-20 09:30:03 +0800

    commit at 2017年 09月 20日 星期三 09:30:01 CST

--- freeMind/Flask.mm
+++ freeMind/Flask.mm
@@ -1,6 +1,10 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1505786635160" ID="ID_45968972" MODIFIED="1505786646688" TEXT="Flask">
-<node CREATED="1505786675116" ID="ID_473091293" MODIFIED="1505786706617" POSITION="right" TEXT="&#x89c6;&#x56fe;&#x51fd;&#x6570;"/>
+<node CREATED="1505786635160" ID="ID_45968972" MODIFIED="1505870324095" STYLE="bubble" TEXT="Flask">
+<node CREATED="1505786675116" ID="ID_473091293" MODIFIED="1505870324096" POSITION="right" STYLE="bubble" TEXT="&#x89c6;&#x56fe;&#x51fd;&#x6570;"/>
+<node CREATED="1505804617115" HGAP="19" ID="ID_251740838" MODIFIED="1505870402412" POSITION="left" STYLE="bubble" TEXT="&#x8def;&#x7531;" VSHIFT="-15">
+<node CREATED="1505804635915" ID="ID_799463334" MODIFIED="1505870324096" STYLE="bubble" TEXT="&#x5904;&#x7406;URL&#x548c;&#x51fd;&#x6570;&#x4e4b;&#x95f4;&#x5173;&#x7cfb;"/>
+<node CREATED="1505870348116" HGAP="42" ID="ID_1006766286" MODIFIED="1505870714851" TEXT="@app.route(&apos;/usr/&lt;int:id&gt;&apos;) &#x5b9a;&#x4e49;&#x8def;&#x7531;" VSHIFT="3"/>
+</node>
 </node>
 </map>
