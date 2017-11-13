45eb3e5378a07c737cd841fbc4da3db5a1300800
whoami <chenke@2xi.com>
2017-11-13 16:30:03 +0800

    commit at 2017年 11月 13日 星期一 16:30:01 CST

---
+++ ubuntu/desktop-init.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# tools for ubuntu desktop 
+apt install vlc terminator docky dia vim fcitx fcitx-table-wubi libreoffice remmina tcpdump virtualbox wiznote atom chromium-browser
+
+#crontab
+"*/30 * * * * sh /home/chenke/work/shell/gitpush_work.sh" 
