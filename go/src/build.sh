f092cd962a522831d650a35553833bc1e5faf9b5
chenke <chenke@ipv6.lan>
2020-04-29 14:07:27 +0800

    commit at 2020年 4月29日 星期三 14时07分25秒 CST

--- go/src/build.sh
+++ go/src/build.sh
@@ -3,7 +3,7 @@ sh /Users/chenke/Documents/worknote/githubUplod.sh
 Main="/data4t/wqyry/check/main"
 checkFile="/data/tmp/main"
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build main.go -o /tmp/main
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o /tmp/main main.go
 scp /tmp/main 10.1.2.188:/tmp/
 \ssh 10.1.2.188 "mv /tmp/main $Main  && chmod +x $Main && $Main"
 \ssh 10.1.2.188 "sh /data4t/wqyry/check/sync.sh"
