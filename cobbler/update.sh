8830dd1aa12094d0171ec81202e93be24543fb9e
chenke <chenke@ipv6.lan>
2020-10-26 09:54:23 +0800

    commit at 2020年10月26日 星期一 09时54分20秒 CST

---
+++ cobbler/update.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#scp from 99 
+IP=172.0.0.99
+for i in Dockerfile start.sh
+do
+   scp ${IP}:/data/cobbler/$i /Users/chenke/Documents/worknote/cobbler
+done
+#update to github
+sh /Users/chenke/Documents/worknote/githubUplod.sh
