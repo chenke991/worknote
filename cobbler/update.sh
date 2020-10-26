89bb383469782a165e5ec9f6e93eb90a9b1c4809
chenke <chenke@ipv6.lan>
2020-10-26 17:24:27 +0800

    commit at 2020年10月26日 星期一 17时24分22秒 CST

--- cobbler/update.sh
+++ cobbler/update.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 #scp from 99 
 IP=172.0.0.199
-for i in  dhcp.template start_in_docker.sh Dockerfile start.sh rmcobbler.sh cdcobbler.sh Dockerfile
+
+for i in  start.sh rmcobbler.sh cdcobbler.sh Dockerfile
 do
    scp -P59878 ${IP}:/data/cobbler/$i /Users/chenke/Documents/worknote/cobbler
 done
 
-scp -r -P59878 ${IP}:/data/cobbler/etc/cobbler/bash /Users/chenke/Documents/worknote/cobbler/
+for i in ubuntu18.04_xiyou.seed  dhcp.template settings start_in_docker.sh
+do
+   scp -P59878 ${IP}:/data/cobbler/etc/cobbler/$i /Users/chenke/Documents/worknote/cobbler/etc/cobbler
+done
+
+#scp -r -P59878 ${IP}:/data/cobbler/etc/cobbler/bash /Users/chenke/Documents/worknote/cobbler/
 
 
 #update to github
